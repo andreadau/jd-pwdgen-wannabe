6a57aaff7379cc75ed9e348e53c51c849a2a600d
andreadau <dauriaandrea99@gmail.com>
2020-11-12 15:53:12 +0100

    ! END !
    Fine jd-pwdgen-wannabe.

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+var userName = prompt("Come ti chiami ?");
+var userSurName = prompt("Il tuo cognome ?");
+var FavouriteColor = prompt("Qual'è il tuo colore preferito ?");
+var number19 = "19";
+document.getElementById('password').innerHTML = userName + userSurName + FavouriteColor + number19;
